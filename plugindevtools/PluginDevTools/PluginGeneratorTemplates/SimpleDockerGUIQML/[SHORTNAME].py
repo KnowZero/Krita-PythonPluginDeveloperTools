77efcf50db67ff96de8b24eb0c98f0718b69b377
KnowZero <KnowZero@users.noreply.github.com>
2023-09-05 01:16:00 +0000

    PluginGenerator: uic fix

--- plugindevtools/PluginDevTools/PluginGeneratorTemplates/SimpleDockerGUIQML/[SHORTNAME].py
+++ plugindevtools/PluginDevTools/PluginGeneratorTemplates/SimpleDockerGUIQML/[SHORTNAME].py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import *
 '''[%AUTOCOMPLETE%]'''
-import uic
+from PyQt5 import uic
 
 class '''[%SHORTNAME%]'''(DockWidget):
 
--- plugindevtools/PluginDevTools/PluginGeneratorTemplates/SimpleExtensionDialogGUIQML/[SHORTNAME].py
+++ plugindevtools/PluginDevTools/PluginGeneratorTemplates/SimpleExtensionDialogGUIQML/[SHORTNAME].py
@@ -1,5 +1,5 @@
 '''[%AUTOCOMPLETE%]'''
-import uic
+from PyQt5 import uic
 
 class '''[%SHORTNAME%]'''Dialog(QDialog):
     
